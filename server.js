1ff1f36971dc774d8eec3cd94d2377efc68a2fcd
filip <s8145@pjwstk.edu.pl>
2013-01-28 10:55:57 +0100

    start2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http'),
     path = require('path'),
     io = require('socket.io');
 
+   
+
 var server = http.createServer(function (req, res) {
     'use strict';
     var filePath = '.' + req.url,
@@ -23,6 +25,8 @@ var server = http.createServer(function (req, res) {
         contentType = 'text/css';
         break;
     }
+
+
 
     path.exists(filePath, function (exists) {
         if (exists) {
@@ -46,22 +50,72 @@ var server = http.createServer(function (req, res) {
 
 var socket = io.listen(server);
 
+/*
+
+Status legenda:
+
+    0 - niewykonane
+    1 - wykonane
+
+ */
+
+var task = [
+    {nazwa: "Zdaj mature", data: "2013-12-12", user: 'filip', status: 0},
+    {nazwa: "Podlej kwiaty", data: "2013-12-12", user: 'filip', status: 1},
+    {nazwa: "Podlej kwiaty", data: "2013-01-26", user: 'monika', status: 0},
+    {nazwa: "Podlej kwiaty", data: "2013-12-12", user: 'monika', status: 1},
+    {nazwa: "Podlej kwiaty", data: "2013-12-12", user: 'grupa', status: 0},
+    {nazwa: "Podlej kwiaty", data: "2013-12-12", user: 'grupa', status: 1}
+];
+
+var users = [
+    {nazwa: 'filip'},
+    {nazwa: 'monika'}
+];
+
+var isset = 0;
+
 socket.on('connection', function (client) {
     'use strict';
-    var username;
+    isset = 0;
 
-    client.send('Wtaj!');
-    client.send('Podaj nazwę użytkownika: ');
-
-    client.on('message', function (msg) {
-        if (!username) {
-            username = msg;
-            client.send('Witaj ' + username + '!');
-            client.broadcast.emit('message', 'Nowy użytkownik: ' + username);
-            return;
+    client.on('setUser', function(data){
+        for(var i=0; i<users.length; i++){
+            if(users[i].nazwa===data){
+                isset = 1;
+            }
         }
-        client.broadcast.emit('message', username + ': ' + msg);
+        if(isset==1){
+            client.emit('yourTask', task);
+        }else{
+            client.emit('intruz', "Intruzom wstep zbroniony!");
+        }
+    }); 
+
+    client.on('addTask', function (data){
+        task.push(data);
+        client.broadcast.emit('newTask', task);
+        client.emit('newTask', task);
     });
+
+    client.on('change', function (data){
+        if(task[data].status===0){
+            task[data].status=1;
+            client.emit('newTask', task);
+            client.broadcast.emit('newTask', task);
+        }else{
+            task[data].status=0;
+            client.emit('newTask', task);
+            client.broadcast.emit('newTask', task);
+        }
+    });
+
+    client.on('delete', function(data){
+        task.splice(data, 1);//Dokumntacja co robi splice => w3c school javascript array object
+        client.emit('newTask', task);
+        client.broadcast.emit('newTask', task);
+    });
+
 });
 
 server.listen(3030);
--- skrypt.js
+++ skrypt.js
@@ -3,7 +3,10 @@
 $(document).ready(function () {
     'use strict';
     var socket = io.connect('http://localhost:3030'),
-        entry_el = $('#entry');
+        myName = '';
+
+    $('#addTask').hide();
+    $('#taska').hide();
 
     console.log('connecting…');
 
@@ -11,20 +14,109 @@ $(document).ready(function () {
         console.log('connected!');
     });
 
-    socket.on('message', function (msg) {
-        var data = msg.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        $('#log ul').append('<li>' + data + '</li>');
-        entry_el.focus();
+    /*
+    
+    rok.miesiac.dzien
+
+     */
+
+    socket.on('intruz', function(msg){
+        $('#login').html('<h1>'+msg+'</h1>')
     });
 
-    entry_el.keypress(function (event) {
-        if (event.keyCode !== 13) {
-            return;
+    socket.on('newTask', function(data){
+        console.log("new task");
+        $('#active').html('');
+        $('#history').html('');
+        var dzisiaj = new Date();
+        var dzisiajStr = dzisiaj.getFullYear() + '-' + dzisiaj.getMonth() + '-' + dzisiaj.getDate();
+        
+		for(var i=0; i<data.length; i++){
+            if(data[i].status===0){
+                var dataZadania = new Date(data[i].data);   
+                var dataZadaniaStr = dataZadania.getFullYear() + '-' + dataZadania.getMonth() + '-' + dataZadania.getDate();
+                if(dzisiajStr>dataZadaniaStr){
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div></li>');
+                    }
+                }else{
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div></li>');
+                    }
+                }
+            }else{
+                $('#history').append('<li id="'+i+'"><div id="pole" class="'+i+'"><p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div><button id="del" class="'+i+'">Usun</button></li>');
+            }   
         }
-        var msg = entry_el.attr('value');
-        if (msg) {
-            socket.send(msg);
-            entry_el.attr('value', '');
-        }
+
+        $("li #pole").click(function (a){
+            socket.emit('change', $(this).attr('class'));
+        });
+
+        $('li #del').click(function (){
+            socket.emit('delete', $(this).attr('class'));
+        });
+
     });
+
+    socket.on('yourTask', function (data){
+        console.log(data.length);
+        var dzisiaj = new Date();
+        var dzisiajStr = dzisiaj.getFullYear() + '-' + dzisiaj.getMonth() + '-' + dzisiaj.getDate();
+        
+		for(var i=0; i<data.length; i++){
+            if(data[i].status===0){
+                var dataZadania = new Date(data[i].data);   
+                var dataZadaniaStr = dataZadania.getFullYear() + '-' + dataZadania.getMonth() + '-' + dataZadania.getDate();
+                if(dzisiajStr>dataZadaniaStr){
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'" class="arv"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div></li>');
+                    }
+                }else{
+                    if(data[i].user===myName){
+                        $('#active').append('<li id="'+i+'"><div id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span class="user">'+data[i].user+'</span></p></div></li>');
+                    }else{
+                        $('#active').append('<li id="'+i+'"><dsiv id="pole" class="'+i+'"<p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div></li>');
+                    }
+                }
+            }else{
+                //append -> dokumentacja jQuery -> dodawanie KOLEJNYCH elementow (nie nadpisywanie)
+                $('#history').append('<li id="'+i+'"><div id="pole" class="'+i+'"><p>'+data[i].nazwa+'<br>'+ data[i].data+'<span>'+data[i].user+'</span></p></div><button id="del" class="'+i+'">Usun</button></li>');
+            }    
+        }
+
+        $('#login').hide();
+        $('#addTask').show();
+        $('#taska').show();
+
+        $("li #pole").click(function (a){
+            socket.emit('change', $(this).attr('class'));
+        });
+
+        $('li #del').click(function (){
+            socket.emit('delete', $(this).attr('class'));
+        });
+
+    });
+
+    $('#zaloguj').click( function (event){
+        socket.emit('setUser', $('#userLogin').val());//Wyemitowanie zdarzenia o nazwie 'setUser', z parametrem z inputa do serwera
+        myName = $('#userLogin').val();
+    });
+
+    $('#add').click(function (event){
+        var nazwa = $('#nazwa').val();
+        var adresat = $('#user').val();
+        var termin= $('#termin').val();
+        var status = 0;
+        socket.emit('addTask', {nazwa: nazwa, data: termin, user: adresat, status: status});
+    });
+
+   
 });
